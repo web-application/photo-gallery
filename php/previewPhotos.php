03b480854c6ba362aa34c77c1c5c3d0b5387bcd8
olgaoskina <oskina.os.spb@ya.ru>
2015-05-12 00:20:51 +0300

    preview photos done (for each user)

--- php/previewPhotos.php
+++ php/previewPhotos.php
@@ -4,28 +4,29 @@
  *  09 April 2015
  */
 include "checkAuthorizedUser.php";
+require_once "admin/connect.php";
+
+$query = "SELECT username, pathToAvatar, 'date', pathToPhoto FROM photos WHERE username='$username'";
+$PATH_TO_IMAGES = "../user_images/";
+$PATH_TO_AVATARS = "../avatars/";
+
+$res = $dbLink->query($query);
+echo '<div class="slider_wrapper">
+    <div id="slider">
+    <ul>
+';
+while ($row = mysqli_fetch_array($res)) {
+echo '
+    <li>
+        <img class="slider_img" src="' . $PATH_TO_IMAGES . $row['pathToPhoto'] . '" alt="Css Template Preview"/>
+    </li>
+';
+}
+echo '</ul>
+    </div>
+</div>';
 ?>
 
-<!--TODO: refactor it-->
-<div class="slider_wrapper">
-    <div id="slider">
-        <ul>
-            <li>
-                <img class="slider_img" src="../user_images/olgaoskina_2015.04.07" alt="Css Template Preview"/>
-            </li>
-            <li>
-                <img class="slider_img" src="../user_images/olgaoskina_2015.04.07_13-59-00" alt="Css Template Preview"/>
-            </li>
-            <li>
-                <img class="slider_img" src="../user_images/olgaoskina_2015.04.07_14-00-00" alt="Css Template Preview"/>
-            </li>
-            <li>
-                <img class="slider_img" src="../user_images/olgaoskina_2015.04.07_14-01-00" alt="Css Template Preview"/>
-            </li>
-            <li>
-                <img class="slider_img" src="../user_images/olgaoskina_2015.04.07_14-02-00" alt="Css Template Preview"/>
-            </li>
-        </ul>
-    </div>
-</div>
+
+
 
