8e82a27a890c228154cbc4858d0258219ddc9330
olgaoskina <oskina.os.spb@ya.ru>
2015-05-12 00:43:33 +0300

    added history

--- php/profileTabbedMenu.php
+++ php/profileTabbedMenu.php
@@ -4,24 +4,34 @@
  *  09 April 2015
  */
 include "checkAuthorizedUser.php";
+require_once "admin/connect.php";
+
+$query = "SELECT username, action, date FROM history WHERE username='$username'";
+$res = $dbLink->query($query);
 ?>
-<div id="wrap" class="wrap_tab right">
-    <div>
+    <div id="wrap" class="wrap_tab right">
         <div>
-            <span onclick="show_tab(1)" id="bat_1" class="myButton"></span>
-            <span onclick="show_tab(2)" id="bat_2" class="myButton"></span>
+            <div>
+                <span onclick="show_tab(1)" id="bat_1" class="myButton"></span>
+                <span onclick="show_tab(2)" id="bat_2" class="myButton"></span>
+            </div>
+        </div>
+        <div style="display: block;" id="tab_1" class="tab">
+            <div style="padding: 5px;">
+                <p>Материалы, разработанные на основе наночастиц с уникальными характеристиками, вытекающими из
+                   микроскопических размеров их составляющих</p>
+            </div>
+        </div>
+        <div id="tab_2" class="tab">
+            <div style="padding: 5px;">
+                <?php
+                while ($row = mysqli_fetch_array($res)) {
+                    echo '
+                    <div class="history-action">' . $row['action'] . '</div>
+                    <div class="history-date">' . $row['date'] . '</div>
+                ';
+                }
+                ?>
+            </div>
         </div>
     </div>
-    <div style="display: block;" id="tab_1" class="tab">
-        <div style="padding: 5px;">
-            <p>Материалы, разработанные на основе наночастиц с уникальными характеристиками, вытекающими из
-               микроскопических размеров их составляющих</p>
-        </div>
-    </div>
-    <div id="tab_2" class="tab">
-        <div style="padding: 5px;">
-            <p>Графен — монослой атомов углерода, полученный в октябре 2004 года в Манчестерском университете (The
-               при комнатной температуре, благодаря чему с решением проблемы формирования.</p>
-        </div>
-    </div>
-</div>
