72429542ff7e5d63b447c3234f8a3ff4f46dadbf
olgaoskina <oskina.os.spb@ya.ru>
2015-05-11 23:23:29 +0300

    refactor urls in footer

--- php/footer.php
+++ php/footer.php
@@ -7,15 +7,15 @@
 <footer class="white">
     <div class="layout-center-wrapper">
         <div class="footer-social">
-            <a href="http://vk.com"
+            <a href="https://vk.com/lelik_stasikovna"
                class="btn-social">
                 <i class="icon-vk"></i>
             </a>
-            <a href="http://vk.com"
+            <a href="https://twitter.com/OskinaOlga"
                class="btn-social">
                 <i class="icon-twitter"></i>
             </a>
-            <a href="http://vk.com"
+            <a href="https://plus.google.com/108126331098709832414/posts"
                class="btn-social">
                 <i class="icon-gplus"></i>
             </a>
